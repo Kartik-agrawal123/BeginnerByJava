d262f64b30511fb78ed5bc8be84f82ec14bb3475
DELL <DELL@DESKTOP-ER888R7>
2022-05-28 18:43:49 +0530

    Added input from user

---
+++ HelloWorld/src/Hello/IFU.java
@@ -0,0 +1,13 @@
+package Hello;
+import java.util.*;
+public class IFU {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		System.out.println(s);
+
+	}
+
+}
